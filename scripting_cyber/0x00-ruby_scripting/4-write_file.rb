4e12c9bfb3b3b82d50166d1b7329541b1d7d81bb
ellen.freeman <ellan.freeman>
2026-05-24 07:31:56 -0400

    24may

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -17,4 +17,7 @@ def merge_json_files(file1_path, file2_path)
   File.open(file2_path, 'w') do |f|
     f.write(JSON.pretty_generate(merged_data))
   end
+
+  # 5. Print the confirmation required by the Holberton checker
+  puts "Merged JSON written to #{file2_path}"
 end

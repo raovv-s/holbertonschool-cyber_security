7118591c45199cd722616a4a0f0dd473653187a8
ellen.freeman <ellan.freeman>
2026-05-24 07:27:01 -0400

    24may

---
+++ scripting_cyber/0x00-ruby_scripting/1-hello_world_class.rb
@@ -0,0 +1,12 @@
+# Class representing a classic Hello World greeting
+class HelloWorld
+  # Constructor method to initialize the instance variable
+  def initialize
+    @message = "Hello, World!"
+  end
+
+  # Method to display the initialized message
+  def print_hello
+    puts @message
+  end
+end
